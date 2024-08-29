6050344a219ac7628b8f15b48a8bcba250da9b5b
projggf@gmail.com <projggf@gmail.com>
2024-08-29 22:02:56 +0000

    stg query for facebook was added

--- models/staging/facebook/stg_ads_creative_facebook_all_data.sql
+++ models/staging/facebook/stg_ads_creative_facebook_all_data.sql
@@ -1,2 +1,53 @@
-select *
+select
+
+    cast(ad_id as string) as ad_id,
+
+    add_to_cart,
+
+    cast(adset_id as string) as adset_id,
+
+    cast(campaign_id as string) as campaign_id,
+
+    channel,
+
+    clicks,
+
+    comments,
+
+    cast(creative_id as string) as creative_id,
+
+    date,
+
+    clicks + comments + likes + shares + views as engagements,
+
+    impressions,
+
+    mobile_app_install as installs,
+
+    likes,
+
+    inline_link_clicks as link_clicks,
+
+    cast(null as string) as placement_id,
+
+    cast(null as int64) as post_click_conversions,
+
+    cast(null as int64) as post_view_conversions,
+
+    cast(null as int64) as posts,
+
+    purchase_value as purchase,
+
+    complete_registration as registrations,
+
+    cast(null as int64) as revenue,
+
+    shares,
+
+    spend,
+
+    purchase as total_conversions,
+
+    cast(null as int64) as video_views
+
 from {{ source('dbt_ysaraykin','src_ads_creative_facebook_all_data') }} 
